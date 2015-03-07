a628a281b7b96c3268bff060c02eb9c0fbb636e1
eringrand <eringrand@gmail.com>
2015-03-07 16:02:31 -0500

    add comments to subset

--- YelpSubSet.R
+++ YelpSubSet.R
@@ -1,16 +1,20 @@
+## Reads in the data
 rvwdf <- read.csv('yelp_academic_dataset_review.csv')
 bizdf <- read.csv('yelp_academic_dataset_business.csv')
 tipdf <- read.csv('yelp_academic_dataset_tip.csv')
 usrdf <- read.csv('yelp_academic_dataset_user.csv')
 chkdf <- read.csv('yelp_academic_dataset_checkin.csv')
-# Subsets the data
+
+## Subsets the data
+
+# Select only the bad data
 rvwdf$BadRating <- ifelse(rvwdf$stars<=1,1,0)
 x <- data.frame(table(bizdf$city))
 x <- x[order(x$Freq),]
 RowCntDf <- data.frame(table(rvwdf$business_id))
 tst <- merge(bizdf,RowCntDf,by.x='business_id','Var1')
-tst$Res <- word_search('Restaurant',tst$categories)
-tst <- tst[,c('categories','Freq','business_id','city','Res')][tst$Res==1,]
+#tst$Res <- word_search('Restaurant',tst$categories)
+#tst <- tst[,c('categories','Freq','business_id','city','Res')][tst$Res==1,]
 x <- sqldf('select city, sum(Freq) as ReviewCount, count(*) as BizCount from tst group by 1')
 x <- x[order(x$ReviewCount,decreasing=T),]
 geos <- as.character(x$city[1:5])
